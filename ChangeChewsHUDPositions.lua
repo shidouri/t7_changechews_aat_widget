f377d179bb9acf9f460a2bee2263bf68806dd21b
shidouri <40169683+shidouri@users.noreply.github.com>
2023-03-03 23:27:04 +0000

    Initial commit - HUD Positions
    Created ChangeChewsHUDPositions.lua. Used for reference for people installing WetEgg's IW Perk Pack (for my Change Chews AAT widget). This file is to be maintained and updated on new HUD releases (DEVRAW, T7Wiki, and other sources will be included).

---
+++ ChangeChewsHUDPositions.lua
@@ -0,0 +1,88 @@
+------------------------------------------------------------------------------------
+-- T7HUD_ZM_FACTORY [ D3V REWRITE ] (uses HudRef instead of self)
+-- Put undernath this line: HudRef.Ammo = AmmoWidget
+
+	local IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+	IWChangeChewsAATDisplay:setLeftRight( false, true, -250, -200 )
+	IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -135 )
+	IWChangeChewsAATDisplay:setZRot(-7)
+	HudRef:addElement(IWChangeChewsAATDisplay)
+	HudRef.IWChangeChewsAATDisplay = IWChangeChewsAATDisplay
+------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------
+-- T7HUD_ZM_FACTORY [ KYLE'S REWRITE ] (uses self instead of HudRef)
+-- Put undernath this line:	self:addElement( self.Ammo )
+
+	self.IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+	self.IWChangeChewsAATDisplay:setLeftRight( false, true, 0, -200 )
+	self.IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -135 )
+    self.IWChangeChewsAATDisplay:setZRot( -7 )
+	self:addElement(self.IWChangeChewsAATDisplay)
+------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------
+-- KYLES IW HUD
+-- Put undernath this line:	self:addElement( self.Ammo )
+
+	self.IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+	self.IWChangeChewsAATDisplay:setLeftRight( false, true, 0, -215 )
+	self.IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -60 )
+	self.IWChangeChewsAATDisplay:setZRot(-7)
+	self:addElement(self.IWChangeChewsAATDisplay)
+------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------
+-- KYLES CS:GO HUD
+-- Put undernath this line:	self:addElement( self.Ammo )
+
+    self.IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+    self.IWChangeChewsAATDisplay:setLeftRight( false, true, 0, -75 )
+    self.IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -180 )
+    self.IWChangeChewsAATDisplay:setZRot( 0 )
+    self:addElement(self.IWChangeChewsAATDisplay)
+------------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------------
+-- KYLES WW2 HUD
+-- Put undernath this line:	self:addElement( self.Ammo )
+
+    self.IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+    self.IWChangeChewsAATDisplay:setLeftRight( false, true, 0, -150 )
+    self.IWChangeChewsAATDisplay:setTopBottom( false, true, 0, 20 )
+    self.IWChangeChewsAATDisplay:setZRot( 0 )
+    self:addElement(self.IWChangeChewsAATDisplay)
+------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------
+-- KYLES BO4 HUD
+-- Put undernath this line:	self:addElement( self.Ammo )
+
+    self.IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+    self.IWChangeChewsAATDisplay:setLeftRight( false, true, 100, -65 )
+    self.IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -58 )
+    self.IWChangeChewsAATDisplay:setZRot( 0 )
+    self:addElement(self.IWChangeChewsAATDisplay)
+------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------
+-- MADGAZ HUD [DEVRAW VERSION]
+-- Put undernath this line: HudRef.Ammo = AmmoWidget
+
+    local IWChangeChewsAATDisplay = CoD.ChangeChewsAATWidget.new(self, controller)
+    IWChangeChewsAATDisplay:setLeftRight( false, true, 0, -88 )
+    IWChangeChewsAATDisplay:setTopBottom( false, true, 0, -75)
+    IWChangeChewsAATDisplay:setZRot(0)
+    HudRef:addElement(IWChangeChewsAATDisplay)
+    HudRef.IWChangeChewsAATDisplay = IWChangeChewsAATDisplay
+------------------------------------------------------------------------------------
